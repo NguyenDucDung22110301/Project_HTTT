5d427d6b1aad39874cb25bd485caaedc0f88c7ba
VUVANDUC2 <vanduc6122003@gmail.com>
2025-04-23 01:39:15 +0700

    Add and Delete Chuc nang Khuyen Mai

--- FinalProject_IS/DAOs/KhuyenMaiDAO.cs
+++ FinalProject_IS/DAOs/KhuyenMaiDAO.cs
@@ -30,7 +30,7 @@ namespace FinalProject_IS.DAOs
                         MaKM = Convert.ToInt32(row["MaKM"]),
                         TenChuongTrinh = row["TenChuongTrinh"].ToString(),
                         GiaTriKhuyenMai = Convert.ToDouble(row["GiaTriKhuyenMai"]),
-                        DieuKienKhuyenMai = row["TenChuongTrinh"].ToString(),
+                        DieuKienKhuyenMai = row["DieuKienKhuyenMai"].ToString(),
                         NgayBatDau = Convert.ToDateTime(row["NgayBatDau"]),
                         NgayKetThuc = Convert.ToDateTime(row["NgayKetThuc"]),
                         SoLuong = Convert.ToInt32(row["SoLuong"])
@@ -40,6 +40,54 @@ namespace FinalProject_IS.DAOs
             }
 
             return dsKhuyenMai;
+        }
+        public static void InsertKhuyenMai(KhuyenMai khuyenMai)
+        {
+            using (SqlConnection conn = new SqlConnection(DataProvider.ConnStr))
+            {
+                string query = @"INSERT INTO KhuyenMai Values(@TenChuongTrinh, @GiaTriKhuyenMai, @DieuKienKhuyenMai,
+                                @NgayBatDau,@NgayKetThuc,@SoLuong)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@TenChuongTrinh", khuyenMai.TenChuongTrinh);
+                    cmd.Parameters.AddWithValue("@GiaTriKhuyenMai", khuyenMai.GiaTriKhuyenMai);
+                    cmd.Parameters.AddWithValue("@DieuKienKhuyenMai", khuyenMai.DieuKienKhuyenMai);
+                    cmd.Parameters.AddWithValue("@NgayBatDau", khuyenMai.NgayBatDau);
+                    cmd.Parameters.AddWithValue("@NgayKetThuc", khuyenMai.NgayKetThuc);
+                    cmd.Parameters.AddWithValue("@SoLuong", khuyenMai.SoLuong);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static int XoaKhuyenMai(int maKM)
+        {
+            int rowsAffected = 0;
+            using (SqlConnection conn = new SqlConnection(DataProvider.ConnStr))
+            {
+                string query = @"DELETE FROM KhuyenMai WHERE MaKM = @MaKM";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@MaKM", maKM);
+
+                    conn.Open();
+                    rowsAffected = cmd.ExecuteNonQuery();
+                }
+            }
+            return rowsAffected;
+
+        }
+
+        
+        public static int XoaNhieuKhuyenMai(List<int> danhSachMaKM)
+        {
+            int rowsAffected = 0;
+            foreach (int maKM in danhSachMaKM)
+            {
+                rowsAffected += XoaKhuyenMai(maKM);
+            }
+            return rowsAffected;
         }
     }
 }
